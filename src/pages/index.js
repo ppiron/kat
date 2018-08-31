97c71431deaa4709360626dbe0bdeafd114ccbd6
Paolo Pironi <ppiron@gmail.com>
2018-08-31 14:28:57 +0100

    added content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,21 @@
 import React from "react"
 import Layout from '../components/layout'
 
-export default () => <Layout></Layout>
+export default () => (
+  <Layout>
+    <h3>Welcome to AllegranDo: Piano Lessons in Edinburgh!</h3>
+		<p>My name is Ekaterina Belik. I am an experienced pianist and piano teacher, professional member of EPTA UK (European Piano Teacher's Association).
+		   I enjoy performing and teaching piano to children and adults in Edinburgh, Scotland.</p>
+		<div id="skills">
+			<p>Studying with me you will:</p>
+			<ul>
+			<li>Enjoy your lessons!</li>
+			<li>Play and read music with confidence</li>
+			<li>Build comprehensive musicianship skills</li>
+			<li>Develop well-coordinated, healthy piano technique</li>
+			<li>Achieve a new level of playing, regardless of age or previous level of accomplishment</li>
+			</ul>	
+			<p>I also help my students to prepare for ABRSM, Trinity and LCM graded exams, Edinburgh Music Festival, various concerts, masterclasses and competitions.</p>
+		</div>
+  </Layout>
+)
