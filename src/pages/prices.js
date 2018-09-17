88c3a838652e30611b849af910edfb3bc84beb2e
Paolo Pironi <ppiron@gmail.com>
2018-09-17 14:48:00 +0100

    Completed prices and testimonials pages

--- src/pages/prices.js
+++ src/pages/prices.js
@@ -1,4 +1,73 @@
 import React from "react"
 import Layout from '../components/layout'
 
-export default () => <Layout></Layout>
+export default () => {
+  return(
+  <Layout>
+    <h3>Prices:</h3>
+    <p>
+      30 min - <span style={{fontWeight: `bold`, fontSize: `18px`, color: `#910015`}}>£17.50</span>, 
+      45 min - <span style={{fontWeight: `bold`, fontSize: `18px`, color: `#910015`}}>£25</span>, 
+      1 hour - <span style={{fontWeight: `bold`, fontSize: `18px`, color: `#910015`}}>£35</span>
+    </p>
+    <h3>Lesson's Policy</h3>
+    <h4>Payment</h4>
+    <ul>
+      <li>
+        Lessons are given <strong>weekly</strong><span style={{fontSize: `16px`}}>&#8270;</span> in <em>30, 45 and 60 minutes lengths</em>.
+       </li>
+      <li>Students are required to commit to at least one lesson per week (4-5 lessons per month). </li>
+      <li><span style={{fontSize: `16px`}}>&#8270;</span> Bi-weekly lessons and one-off consultations are <em>by special arrangement during off-peak times only</em>. Please note that your time slot can not be guaranteed as priority will be given to students who take lessons weekly.</li>
+      <li><strong>Tuition is paid one month in advance on the 1st lesson of each month</strong>. Tuition payment covers your private lesson weekly slot, as well as teacher’s time spent planning each lesson, researching music and learning pieces to be taught, attending professional meetings, communicating with students/parents outside of lessons, coordinating lessons, scheduling events; piano tuning and Piano Teachers’ Organisation fees.</li>
+      <li>Fees for exams, festivals and other events are <em>charged separately</em> in addition to tuition.</li>
+      <li>Payment for lessons can be made by <em>cheque, bank transfer or cash</em>.</li>
+    </ul>
+    <h4>Cancellations/Make-up lessons</h4>
+    <ul>
+      <li>Students are expected to attend each scheduled lesson. <strong>No refunds will be given for lessons missed by the student</strong>. Please understand that the tuition you pay reserves an exclusive weekly time in my schedule. If a student does not attend a lesson I will then use that time to plan for the student’s progress, locate materials, etc. Unlike many service professionals, I cannot resell that time nor can I create extra teaching time in which to give lessons. </li>
+      <li>No payment credit will be provided for students who miss lessons for any reason.</li>
+      <li>Any lesson that is cancelled by the teacher will be made up or a credit given on your payment.</li>
+      <li>Make-up lessons will be offered if at least 48 hours’ notice is provided or for honest cases of illness or emergency. Emergency situations will be handled on a case-by-case basis. Students will be given 2-3 options of a possible make-up time (make-up lessons will be scheduled during the normal teaching hours of the teacher), however, if the student’s schedule does not permit for the make-up lesson, the lesson is a forfeit. I will try to reschedule your lesson at the earliest possible date. That may result in having two lessons within the same week or a double lesson. Missed lessons can only be made-up within 30 days from missed date. If a scheduled make-up lesson is missed or not made up within 30 days, a second one cannot be rescheduled. Only 2 make-up lessons will be offered per term.</li>
+    </ul>
+    <h4>Interruptions</h4>
+    <ul>
+      <li>
+        Piano lessons are given weekly throughout the school year. Periods of interruptions to lessons, such as school, or any other holidays should be notified at least 4 weeks in advance. For scheduled absences of a month or more, payment will not be required however the timetable slot for your lesson will not be retained for you.
+      </li>
+    </ul>
+    <h4>Drop Off/Pick up</h4>
+    <ul>
+      <li>
+        Prompt drop off and pick up is expected. If you arrive late to your lesson, your lesson will still end at its scheduled time. This ensures that the remaining lessons for the day will be on time. Students may not be left more than 5 minutes after their lesson time. Please do not come early to your lessons as this may disturb the lesson before yours.
+      </li>
+    </ul>
+    <h4>Termination of Lessons</h4>
+    <p>
+      One month notice is required for discontinuation of lessons
+      <br/> 
+      I reserve the right to terminate lessons for any of the following reasons
+    </p>
+    <ol>
+      <li>Student shows lack of progress due to insufficient practice</li>
+      <li>Unfulfilled requirements or not making an effort to improve</li>
+      <li>Persistent absences from lessons.</li>
+      <li>Repeated failure to pay tuition when due.</li>
+    </ol>
+    <h4>Instrument and Lesson Materials</h4>
+    <ul>
+      <li>All students are required to have a piano at home for daily practice. A regularly tuned, acoustic piano is highly recommended for intermediate and advanced level students. </li>
+      <li>The student will purchase their own music books as advised by the teacher. Any materials given out freely by the teacher will be subject to a nominal charge, if they need to be replaced due to loss or damage by the student. This charge will cover the cost of any materials used and the teacher’s time in replacing the materials.</li>
+    </ul>
+    <h4>Student's Practice</h4>
+    <ul>
+      <li>
+        Students are expected to carry out their practice and assignments daily, at least 5 days a week. There will be specific directions outlined in the student’s notebook. Parents are encouraged to monitor the children’s practice and sign off the practice sheet on completion of tasks.
+      </li>
+    </ul>
+    <h4>Public Performances</h4>
+    <ul>
+      <li>Student’s participation in concerts, recitals, competitions and other events is strongly encouraged but not required. </li>
+      <li>No performances, whether public, in competitions, festivals or examinations, shall be given without the consent of the teacher, and the teacher will not submit the name of any pupil for any such performances without prior consent.</li>
+    </ul>
+  </Layout>
+)}
--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,4 +1,45 @@
 import React from "react"
 import Layout from '../components/layout'
 
-export default () => <Layout></Layout>
+export default () => {
+  return(
+    <Layout>
+      <h3>Testimonials</h3>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`}}>
+          Ms Belik is a most effective piano teacher, who has a thorough and detailed knowledge of pedagogic issues. She involves a wide range of techniques in her teaching and these enthuse and stimulate her pupils well. Her pupils make very good progress and achieve well in examinations.
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          John Treherne Head of Gateshead Music Service, CT ABRSM Course Leader and Examiner
+        </p>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`, marginTop: `30px`}}>
+          Thank you so much for my wonderful piano lessons. I have improved a lot since I started lessons with you. You are a fantastic teacher!
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          Marcela Valenzuela
+        </p>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`, marginTop: `30px`}}>
+          My 5-year-old daughter has been taking lessons from Katya for about 2 years. The lessons are fun and exciting and I am happy with her progress and the patience Katya has in coaching her.
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          Angela
+        </p>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`, marginTop: `30px`}}>
+          Katya is a wonderful person, her good sense of humour and flexibility always make lessons enjoyable. Most importantly, she always has the patience to make sure that you are doing everything right, and that she answers any question that you may have.
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          Anna
+        </p>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`, marginTop: `30px`}}>
+          I have learnt more in six months with Katya than five years with other teachers.
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          Andrew
+        </p>
+        <p style={{paddingLeft: `50px`, fontStyle: `italic`, color: `black`, marginTop: `30px`}}>
+          Ekaterina has a talent for working with students of all ages and abilities. She is able to make learning fun for the young students, and does a great job of keeping her students engaged and interested as they move up.
+        </p>
+        <p style={{fontSize: `12px`, fontWeight: `bold`, textAlign: `right`}}>
+          Irina Koroleva Head of Piano Department at the Children's Art School
+        </p>
+    </Layout>
+  )}
