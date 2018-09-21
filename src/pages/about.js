e3adc14744fa6ff3269668e3fda268b04d4b3cba
Paolo Pironi <ppiron@gmail.com>
2018-09-21 14:07:36 +0100

    Added about and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,16 @@
 import React from "react"
 import Layout from '../components/layout'
+import me from '../images/me2.jpg'
+import './about.css'
 
-export default () => <Layout></Layout>
+export default () => {
+  return(
+    <Layout>
+      <div className='container'>
+        <img className='me' src={me} alt="me at the piano"/>
+        <p className='about'>
+          Ekaterina is an enthusiastic piano teacher and performer with over 20 years experience in both education and private practice. Moscow educated in Music and Educational Psychology she furthered her studies and attained CT ABRSM (Certificate of Teaching from Associated Board of the Royal Schools of Music) in 2009. Ekaterina is a full professional member of EPTA UK (European Piano Teachers Association) and teaches different ages and stages to competition level. Her students regularly perform at Edinburgh Music Festival, EPTA concerts and masterclasses and consistently achieve Distinction and Merit levels in their graded examinations.
+        </p>
+      </div>
+    </Layout>
+  )}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,32 @@
 import React from "react"
 import Layout from '../components/layout'
+import './contact.css'
 
-export default () => <Layout></Layout>
+export default () => {
+  return(
+    <Layout>
+      <h3>
+        Contact details:
+      </h3>
+      <p>
+        Telephone: 07961 619168, Address: 69 Carnbee Park EH16 6GG
+      </p>
+      <h3>
+        You can also use the below form to contact me:
+      </h3>
+      <div className='formContainer'>
+        <form action=''>
+          <div className='subject'>
+            <label htmlFor='subject'>Subject:</label>
+            <input type='text' name='subject' placeholder='Subject' />
+          </div>
+          <div className='email'>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' name='email' placeholder='Your email' />
+          </div>
+          {/* <label htmlFor='content'>Text:</label> */}
+          <textarea name="content" placeholder='Your message'></textarea><button className='button' type="submit">Submit</button>
+        </form>
+      </div>
+    </Layout>
+  )}
